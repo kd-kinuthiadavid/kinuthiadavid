983e823430444579513a4d47b853c10e934d20d3
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2023-09-21 22:09:57 +0300

    feat: create the intro

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "kd-soft-pink": "#F8BDC4",
+        "kd-lime": "#DEF6CA",
+        "kd-yellow": "#FAD6A2",
+        "kd-hard-pink": "#c7386d",
+        "kd-primary": "#002350",
+        "kd-white": "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
